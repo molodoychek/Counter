a57e5f74b5116f5dfc228a22b1ae66513c6e3700
molodoy_chek <a.100proc@gmail.com>
2025-11-17 23:30:12 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Александр on 17.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
